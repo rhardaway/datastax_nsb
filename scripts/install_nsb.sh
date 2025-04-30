f508e610fb732b1ef07fd8f21a7ac4f37efe89af
rhardaway <robert.hardaway@gmail.com>
2025-04-29 21:00:52 -0600

    bug in nsb latest

--- scripts/install_nsb.sh
+++ scripts/install_nsb.sh
@@ -3,7 +3,9 @@
 echo ""
 echo "get nosqlbench"
 
-curl -L -O https://github.com/nosqlbench/nosqlbench/releases/latest/download/nb5 --output-dir "/home/ubuntu/datastax_nsb"
+## rolling back nsb version
+##curl -L -O https://github.com/nosqlbench/nosqlbench/releases/latest/download/nb5
+curl -L -O  https://github.com/nosqlbench/nosqlbench/releases/download/5.21.3-release/nb5
 chmod u+x /home/ubuntu/datastax_nsb/nb5
 
 sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y universe
